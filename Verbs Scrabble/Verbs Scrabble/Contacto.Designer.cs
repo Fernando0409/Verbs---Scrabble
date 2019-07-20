c31afb19a92538a0bb82ed4cf81ad07627cacf6e
Luis Fernando Martinez Silva <fernando04_software@outlook.com>
2019-07-20 12:46:02 -0500

    Diseño de la sección "Contactanos"
    Diseño completado de la sección; se agregaron 2 campos (email y sugerencia) ademas de un botón para enviar el correo. Funcionalidad al 15%

--- Verbs Scrabble/Verbs Scrabble/Contacto.Designer.cs
+++ Verbs Scrabble/Verbs Scrabble/Contacto.Designer.cs
@@ -29,22 +29,81 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSugerence = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(271, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.Size = new System.Drawing.Size(132, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Contactanos";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(281, 325);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(112, 45);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingresa tu duda/sugerencia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Correo Electronico";
+            // 
+            // txtSugerence
+            // 
+            this.txtSugerence.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSugerence.Location = new System.Drawing.Point(42, 181);
+            this.txtSugerence.Multiline = true;
+            this.txtSugerence.Name = "txtSugerence";
+            this.txtSugerence.Size = new System.Drawing.Size(552, 123);
+            this.txtSugerence.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(42, 101);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(272, 33);
+            this.txtEmail.TabIndex = 7;
             // 
             // Contacto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSugerence);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSend);
             this.Controls.Add(this.label1);
             this.Name = "Contacto";
             this.Size = new System.Drawing.Size(667, 396);
@@ -56,5 +115,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSugerence;
+        private System.Windows.Forms.TextBox txtEmail;
     }
 }
--- Verbs Scrabble/Verbs Scrabble/Contacto.cs
+++ Verbs Scrabble/Verbs Scrabble/Contacto.cs
@@ -16,5 +16,22 @@ namespace Verbs_Scrabble
         {
             InitializeComponent();
         }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(txtEmail.Text == ""  || txtSugerence.Text == "")
+            {
+                MessageBox.Show("Se detecto que uno de los campos se encuentran vacios, rellenelos", "Notificación",
+                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtEmail.Focus();
+            }
+            else
+            {
+                txtEmail.Text = "";
+                txtSugerence.Text = "";
+                MessageBox.Show("Gracias por tu comentario, trabajamos contantemente para tu satisfaccion",
+                    "Notificación", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
     }
 }
