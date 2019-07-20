b6d24c6c05543c9edaef147dd3d402c39490de72
Luis Fernando Martinez Silva <fernando04_software@outlook.com>
2019-07-20 14:37:04 -0500

    Creacion y finalizacion del diseño de la seccion "Verbs"
    Se crea una nueva seccion para el juego la cual contendra la funcion principal del juego; mostrar los verbos, actualizarlos, mostrar alertas, etc.
    
    Funcionalidad: 0%

---
+++ Verbs Scrabble/Verbs Scrabble/VerbsForm.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Verbs_Scrabble
+{
+    partial class VerbsForm
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMistakes = new System.Windows.Forms.Label();
+            this.lblAcerts = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureVerb = new System.Windows.Forms.PictureBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtVerb = new System.Windows.Forms.TextBox();
+            this.lblVerb2 = new System.Windows.Forms.Label();
+            this.lblVerb1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVerb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMistakes
+            // 
+            this.lblMistakes.AutoSize = true;
+            this.lblMistakes.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMistakes.Location = new System.Drawing.Point(606, 66);
+            this.lblMistakes.Name = "lblMistakes";
+            this.lblMistakes.Size = new System.Drawing.Size(18, 19);
+            this.lblMistakes.TabIndex = 33;
+            this.lblMistakes.Text = "0";
+            // 
+            // lblAcerts
+            // 
+            this.lblAcerts.AutoSize = true;
+            this.lblAcerts.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAcerts.Location = new System.Drawing.Point(606, 47);
+            this.lblAcerts.Name = "lblAcerts";
+            this.lblAcerts.Size = new System.Drawing.Size(18, 19);
+            this.lblAcerts.TabIndex = 32;
+            this.lblAcerts.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(523, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 19);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Mistakes :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(539, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 19);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Acerts :";
+            // 
+            // pictureVerb
+            // 
+            this.pictureVerb.Location = new System.Drawing.Point(455, 104);
+            this.pictureVerb.Name = "pictureVerb";
+            this.pictureVerb.Size = new System.Drawing.Size(157, 161);
+            this.pictureVerb.TabIndex = 29;
+            this.pictureVerb.TabStop = false;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(215, 28);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(190, 28);
+            this.lblCategoria.TabIndex = 28;
+            this.lblCategoria.Text = "Opcion Categoria";
+            this.lblCategoria.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(606, 28);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(18, 19);
+            this.lblScore.TabIndex = 27;
+            this.lblScore.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(544, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 19);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Score :";
+            // 
+            // txtVerb
+            // 
+            this.txtVerb.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVerb.Location = new System.Drawing.Point(162, 250);
+            this.txtVerb.MaxLength = 20;
+            this.txtVerb.Multiline = true;
+            this.txtVerb.Name = "txtVerb";
+            this.txtVerb.Size = new System.Drawing.Size(145, 23);
+            this.txtVerb.TabIndex = 19;
+            // 
+            // lblVerb2
+            // 
+            this.lblVerb2.AutoSize = true;
+            this.lblVerb2.Font = new System.Drawing.Font("Cambria", 12F);
+            this.lblVerb2.Location = new System.Drawing.Point(158, 174);
+            this.lblVerb2.Name = "lblVerb2";
+            this.lblVerb2.Size = new System.Drawing.Size(55, 19);
+            this.lblVerb2.TabIndex = 25;
+            this.lblVerb2.Text = "Verb 2";
+            // 
+            // lblVerb1
+            // 
+            this.lblVerb1.AutoSize = true;
+            this.lblVerb1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVerb1.Location = new System.Drawing.Point(158, 104);
+            this.lblVerb1.Name = "lblVerb1";
+            this.lblVerb1.Size = new System.Drawing.Size(55, 19);
+            this.lblVerb1.TabIndex = 24;
+            this.lblVerb1.Text = "Verb 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.label3.Location = new System.Drawing.Point(43, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 22);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "label3 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.label2.Location = new System.Drawing.Point(43, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 22);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "label2 :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 22);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "label1 :";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(455, 319);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(145, 49);
+            this.btnNext.TabIndex = 20;
+            this.btnNext.Text = "&Continuar";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // VerbsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblMistakes);
+            this.Controls.Add(this.lblAcerts);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureVerb);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtVerb);
+            this.Controls.Add(this.lblVerb2);
+            this.Controls.Add(this.lblVerb1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNext);
+            this.Name = "VerbsForm";
+            this.Size = new System.Drawing.Size(667, 396);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVerb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMistakes;
+        private System.Windows.Forms.Label lblAcerts;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureVerb;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtVerb;
+        private System.Windows.Forms.Label lblVerb2;
+        private System.Windows.Forms.Label lblVerb1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
