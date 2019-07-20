9b1eafecd896b58e76d6e09554d3b6adfc696dd3
Luis Fernando Martinez Silva <fernando04_software@outlook.com>
2019-07-20 13:51:09 -0500

    Finalizacion del diseño de la seccion "Creditos"
    El diseño de la sección de "Creditos" tiene un progreso del 100%. En esta se ha indicado las personas que han colaborado a lo largo del proyecto y un enlace para que se unan mas personas al proyecto.
    
    Se deja unicamente pendiente la funcionalidad del mismo (enviar a la pagina del repositorio)

--- Verbs Scrabble/Verbs Scrabble/Creditos.Designer.cs
+++ Verbs Scrabble/Verbs Scrabble/Creditos.Designer.cs
@@ -28,23 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Creditos));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkJoin = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(207, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 25);
+            this.label1.Size = new System.Drawing.Size(279, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Creditos - Agradecimientos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(28, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(603, 228);
+            this.label2.TabIndex = 4;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // linkJoin
+            // 
+            this.linkJoin.AutoSize = true;
+            this.linkJoin.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkJoin.Location = new System.Drawing.Point(423, 337);
+            this.linkJoin.Name = "linkJoin";
+            this.linkJoin.Size = new System.Drawing.Size(208, 38);
+            this.linkJoin.TabIndex = 1;
+            this.linkJoin.TabStop = true;
+            this.linkJoin.Text = "Eres desarrollador o deseas \r\ndonarnos? Unete!\r\n";
             // 
             // Creditos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.linkJoin);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Creditos";
             this.Size = new System.Drawing.Size(667, 396);
@@ -56,5 +84,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkJoin;
     }
 }
