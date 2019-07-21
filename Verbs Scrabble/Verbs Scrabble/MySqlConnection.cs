17c7e35d533cc73bdc953f217237f4ed9aae7bdb
Luis Fernando Martinez Silva <fernando04_software@outlook.com>
2019-07-21 17:51:13 -0500

    Creacion de la clase "MySqlConnection"
    Clase encargada de establecer conexión con el servidor. De igual forma recupera los verbos en cada uno de los tiempos para su posterior proyección

---
+++ Verbs Scrabble/Verbs Scrabble/MySqlConnection.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Verbs_Scrabble
+{
+    class MySqlConnection
+    {
+        public static string source = "datasource=localhost; port=3306; username=root; password=; database=verbsscrabble2";
+        public static MySql.Data.MySqlClient.MySqlConnection dbConection = new MySql.Data.MySqlClient.MySqlConnection(source);
+        public static MySqlDataReader verbs_infinitive, verbs_simple_past, verbs_past_participle;
+
+        private int idVerb = 0;
+
+        public static bool OpenConnection()
+        {
+            try
+            {
+                dbConection.Open();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo establecer conexion con el servidor: " + ex.Message, "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public static bool CloseConnection()
+        {
+            try
+            {
+                dbConection.Close();
+                return true;
+
+            } catch(Exception e)
+            {
+                MessageBox.Show("No se pudo cerrar conexion con el servidor: " + e.Message, "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public void setID()
+        {
+            Random random = new Random();
+            idVerb = random.Next(1, 101);
+        }
+
+        public string getVerbInfinitive()
+        {
+            string infinitive = "";
+            OpenConection();
+            string sql = "SELECT verb FROM verbs_inifnitive WHERE id = '" + idVerb + "'";
+            MySqlCommand mySql = new MySqlCommand(sql, dbConection);
+
+            try
+            {
+                verbs_infinitive = mySql.ExecuteReader();
+                if (verbs_infinitive.HasRows)
+                {
+                    while (verbs_infinitive.Read())
+                        infinitive = Convert.ToString(verbs_infinitive["verb"]);
+                }
+            }catch(Exception e)
+            {
+                MessageBox.Show("No se pudo procesar la peticion, lo sentimos: " + e.Message, "Notificación",
+                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            CloseConection();
+            return infinitive;
+        }
+
+        public string getVerbSP()
+        {
+            string sp = "";
+            OpenConection();
+            string sql = "SELECT verb FROM verbs_simple_past WHERE id = '" + idVerb + "'";
+            MySqlCommand mySql = new MySqlCommand(sql, dbConection);
+
+            try
+            {
+                verbs_simple_past = mySql.ExecuteReader();
+                if (verbs_simple_past.HasRows)
+                {
+                    while (verbs_simple_past.Read())
+                        sp = Convert.ToString(verbs_simple_past["verb"]);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("No se pudo procesar la peticion, lo sentimos: " + e.Message, "Notificación",
+                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            CloseConection();   
+            return sp;
+        }
+
+        public string getVerbPP()
+        {
+            string pp = "";
+            OpenConection();
+            string sql = "SELECT verb FROM verbs_inifnitive WHERE id = '" + idVerb + "'";
+            MySqlCommand mySql = new MySqlCommand(sql, dbConection);
+            try
+            {
+                verbs_past_participle = mySql.ExecuteReader();
+                if (verbs_past_participle.HasRows)
+                {
+                    while (verbs_past_participle.Read())
+                        pp = Convert.ToString(verbs_past_participle["verb"]);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("No se pudo procesar la peticion, lo sentimos: " + e.Message, "Notificación",
+                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            CloseConection();
+            return pp;
+        }
+    }
+}
